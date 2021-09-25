36cb7dfdef4e7f7532eb087e8069d645a3a1e596
zhengyuekai <zhengyuekai@hh-medic.com>
2021-09-26 00:00:13 +0800

    提交

---
+++ src/AdapterPattern/CommissionTest/Adapte220To18.java
@@ -0,0 +1,15 @@
+package AdapterPattern.CommissionTest;
+
+public class Adapte220To18 {
+
+
+private Use220 use220;
+
+    public Adapte220To18(Use220 use220) {
+        this.use220 = use220;
+    }
+
+    private void use18V(){
+        use220.use2201();
+    }
+}
---
+++ src/AdapterPattern/CommissionTest/Use220.java
@@ -0,0 +1,8 @@
+package AdapterPattern.CommissionTest;
+
+public class Use220 {
+
+    public void use2201(){
+        System.out.println("使用220v");
+    }
+}
